0e84e149d1313355fbb0a03e37749bf97a1868fb
yunjinghui <1432680302@qq.com>
2023-11-27 16:32:54 +0800

    单例

--- YJHOCExtTools/YJHOCExtTools.m
+++ YJHOCExtTools/YJHOCExtTools.m
@@ -9,8 +9,30 @@
 #import "YJHOCExtTools+machineModel.h"
 
 @implementation YJHOCExtTools
+
++ (instancetype)sharedManager {
+    static YJHOCExtTools *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [self manager];
+    });
+    return _sharedManager;
+}
+
++ (instancetype)manager {
+    return [[self alloc] init];;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+
+    }
+    return self;
+}
+
 // 手机型号
 + (NSString *)machineModel {
-    return [YJHOCExtTools new].machinePlatform;
+    return [YJHOCExtTools sharedManager].machinePlatform;
 }
 @end
